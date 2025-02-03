bdda32b81b48d58e2f5226edc86cc63cbfbfbbe1
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2025-02-03 16:41:04 +0100

    Project: fertiggestellt (bis auf mobile)

--- assets/projects.js
+++ assets/projects.js
@@ -10,16 +10,53 @@ const projects = [
         id: "moc1", 
         name: "Masters of Crafters 1", 
         numScreenshots: 15,
-        startTimestamp: 0,
-        endTimestamp: 10,
-        players: {
+        startTimestamp: 1586037600,
+        endTimestamp: 1610924400,
+        accounts: {
+            players: -1,
             java: 67,
-            bedrock: -1,
+            bedrock: -1
         },
-        downloads: [
-            {name: "Overworld", url: "https://www.mediafire.com/file/7z1z1z1z1z1z1z1/Masters_of_Crafters_1.zip/file"},
-            {name: "Nether", url: "https://www.mediafire.com/file/7z1z1z1z1z1z1z1/Masters_of_Crafters_1_Nether.zip/file"},
-            {name: "End", url: "https://www.mediafire.com/file/7z1z1z1z1z1z1z1/Masters_of_Crafters_1_End.zip/file"}
-        ]
-    }
+        download: "https://kdrive.infomaniak.com/app/share/861788/6df5c095-d509-4259-9567-3995740d495c/files/20814",
+    },
+    {
+        id: "moc2", 
+        name: "Masters of Crafters 2", 
+        numScreenshots: 15,
+        startTimestamp: 1624658400,
+        endTimestamp: 1636758000,
+        accounts: {
+            players: -1,
+            java: 69,
+            bedrock: 27
+        },
+        download: "https://kdrive.infomaniak.com/app/share/861788/6df5c095-d509-4259-9567-3995740d495c/files/20815",
+    },
+    {
+        id: "moc3", 
+        name: "Masters of Crafters 3", 
+        numScreenshots: 15,
+        startTimestamp: 1659650400,
+        endTimestamp: 1669417200,
+        accounts: {
+            players: -1,
+            java: 81,
+            bedrock: 14
+        },
+        download: "https://kdrive.infomaniak.com/app/share/861788/6df5c095-d509-4259-9567-3995740d495c/files/20816",
+    },
+    {
+        id: "moc4", 
+        name: "Masters of Crafters 4", 
+        numScreenshots: 12,
+        startTimestamp: 1699657200,
+        endTimestamp: 1729288800,
+        accounts: {
+            players: 95,
+            java: 87,
+            bedrock: 20
+        },
+        download: "https://kdrive.infomaniak.com/app/share/861788/6df5c095-d509-4259-9567-3995740d495c/files/51921",
+    },
+
 ];
--- project/script.js
+++ project/script.js
@@ -1,52 +1,76 @@
-var project = "moc1";
+var project = "moc1";   // project id (default: moc1)
+var screenshotN = 1;    // screenshot slider start index (default: 1)
 
 document.addEventListener('DOMContentLoaded', function() {
     checkHash();
     window.addEventListener('hashchange', checkHash);
-
-    // let bgScreenshot = Math.floor(Math.random() * screenshots[moc-1]) + 1
-    // document.getElementById('backgroundImage').style.backgroundImage = `url(../assets/screenshots/moc${moc}/${bgScreenshot}.webp)`;
-
-    // // activate animations if element is in view
-    // let observer = new IntersectionObserver(entries => {
-    //     entries.forEach(entry => {
-
-    //         if (entry.intersectionRatio > 0) {
-    //             entry.target.classList.remove('paused');
-    //         } else {
-    //             entry.target.classList.add('paused');
-    //         }
-    //     });
-    // });
-
-    // // observe paused elements
-    // document.querySelectorAll('.paused').forEach(el => {
-    //     observer.observe(el);
-    // });
 });
 
 function checkHash() {
     // get project name from url
     if (window.location.hash) {
-        moc = window.location.hash.substring(1);
+        hash = window.location.hash.substring(1);
     } else window.location.href = '#'+projects[projects.length - 1].id;
 
     // set Logo
-    document.getElementById('logoImg').src = `../assets/logos/${moc}.png`;
+    document.getElementById('logoImg').src = `../assets/logos/${hash}.png`;
 
     // define project
-    project = projects.find(project => project.id === moc);
+    project = projects.find(project => project.id === hash);
 
     // set title
     document.getElementById('title').textContent = project.name;
+    
+    // set download link
+    document.getElementById('downloadButton').href = project.download;
+
+    // set background image
+    let bgScreenshot = Math.floor(Math.random() * project.numScreenshots) + 1
+    document.getElementById('backgroundImage').style.backgroundImage = `url(../assets/screenshots/${hash}/${bgScreenshot}.webp)`;
+
+    // set screenshot
+    document.getElementById("screenshotRight").addEventListener('click', () => screenshot(1, project));
+    document.getElementById("screenshotLeft").addEventListener('click', () => screenshot(-1, project));
+    screenshot(0, project);
+
+    // set duration
+    document.getElementById('startDate').textContent = new Date(project.startTimestamp * 1000).toLocaleDateString(undefined, {day: '2-digit', month: '2-digit', year: 'numeric'});
+    document.getElementById('endDate').textContent = new Date(project.endTimestamp * 1000).toLocaleDateString(undefined, {day: '2-digit', month: '2-digit', year: 'numeric'});
+    document.getElementById('duration').textContent = Math.floor((project.endTimestamp - project.startTimestamp) / 86400) + " days";
+
+    // set players
+        // set text
+    document.getElementById('totalPlayers').textContent = (project.accounts.players < 0 
+        ? (project.accounts.java + (project.accounts.bedrock < 0 ? 0 : project.accounts.bedrock)) 
+        : project.accounts.players)
+    document.getElementById('javaPlayers').textContent = project.accounts.java < 0 ? "Wurde nicht unterstützt" : project.accounts.java;
+    document.getElementById('bedrockPlayers').textContent = project.accounts.bedrock < 0 ? "Wurde nicht unterstützt" : project.accounts.bedrock;
+        // set rect width
+    document.getElementById('totalPlayersRect').style.width = (project.accounts.players < 0 
+        ? (project.accounts.java + (project.accounts.bedrock < 0 ? 0 : project.accounts.bedrock) + 40) 
+        : project.accounts.players + 20)  
+        + "px";
+    document.getElementById('javaPlayersRect').style.width = project.accounts.java < 0 ? 0 : project.accounts.java + 20 +"px";
+    document.getElementById('bedrockPlayersRect').style.width = project.accounts.bedrock < 0 ? 0 : project.accounts.bedrock + 20 +"px";
+        // set special text
+    if (project.accounts.players > 0) {
+        document.getElementById('totalText').textContent = "Verschiedene Spieler";
+    }
+    if (project.accounts.java < 0) {
+        document.getElementById('javaPlayers').classList.add('empty');
+    }
+    if (project.accounts.bedrock < 0) {
+        document.getElementById('bedrockPlayers').classList.add('empty');
+    }
+
 }
 
 
 
-// function screenshot(direction) {
-//     screenshotN += direction;
-//     if (screenshotN > screenshots[moc-1]) screenshotN = 1;
-//     if (screenshotN < 1) screenshotN = screenshots[moc-1];
-
-//     document.getElementById('screenshot').src = `../assets/screenshots/moc${moc}/${screenshotN}.webp`;
-// }
+function screenshot(direction, project) {
+    screenshotN += direction; 
+    
+    if (screenshotN > project.numScreenshots) screenshotN = 1;
+    if (screenshotN < 1) screenshotN = project.numScreenshots;
+    document.getElementById('screenshot').src = `../assets/screenshots/${project.id}/${screenshotN}.webp`;
+}
